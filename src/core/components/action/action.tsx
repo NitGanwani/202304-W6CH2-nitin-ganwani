57c0271d255348e0572e3c541e9c829a8d763f89
Nitin Ganwani <nitganwani@gmail.com>
2023-06-06 21:35:52 +0200

    Add functionality to hang and call buttons

--- src/core/components/action/action.tsx
+++ src/core/components/action/action.tsx
@@ -1,7 +1,16 @@
 import { usePhone } from "../../../hooks/use.phone";
 
 export function Action() {
-  const { iscalling, screen } = usePhone();
+  const { iscalling, screen, handleHang, handleCall } = usePhone();
+
+  function handleHangClick() {
+    handleHang();
+  }
+
+  function handleCallClick() {
+    handleCall();
+  }
+
   return (
     <>
       <a
@@ -9,10 +18,15 @@ export function Action() {
         className={`call ${
           iscalling ? "" : screen.length === 9 ? "active" : ""
         }`}
+        onClick={handleCallClick}
       >
         Call
       </a>
-      <a href="#" className={`hang ${iscalling ? "active" : ""}`}>
+      <a
+        href="#"
+        className={`hang ${iscalling ? "active" : ""}`}
+        onClick={handleHangClick}
+      >
         Hang Up
       </a>
     </>
